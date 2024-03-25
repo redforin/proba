df3c12653754aa2aa40fd50760cadc29c03bc3af
redforin <galakxova24@gmail.com>
2024-03-25 20:07:03 +0300

    proba1

--- app/src/main/java/com/example/proba/RegisterActivity.java
+++ app/src/main/java/com/example/proba/RegisterActivity.java
@@ -3,8 +3,14 @@ package com.example.proba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText email_register;
+    private EditText password_register;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
